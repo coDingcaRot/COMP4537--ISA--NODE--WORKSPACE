83681fe2f146c35c7e4c014a6aaf2589ce442bcd
CC <89727825+coDingcaRot@users.noreply.github.com>
2025-02-08 12:24:02 -0800

    Re added git module and change submodule path

--- COMP4537/labs/4/api_post.js
+++
@@ -1,9 +0,0 @@
-//require dictionary to add stuff into it
-
-//Handles Post request and adding words into dictionary from req.body
-exports.post = (req, res) => {
-    console.log('Inside Post call');
-
-    console.log(req.body); // cant use this until we write the header and we are recieving an application/json
-    console.log(req.method);
-};
